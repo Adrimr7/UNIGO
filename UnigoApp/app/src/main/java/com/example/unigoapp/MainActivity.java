9172fa33daf9ea235beb402b2ee28ab99799afde
Adrian Mena Ruiz <adrianmenaruiz@gmail.com>
2025-05-05 20:25:09 +0200

    Cambios varios, cambio de idioma

--- UnigoApp/app/src/main/java/com/example/unigoapp/MainActivity.java
+++ UnigoApp/app/src/main/java/com/example/unigoapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.unigoapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.unigoapp.databinding.ActivityMainBinding;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     // se ha usado la plantilla de la navigation view.
@@ -21,9 +28,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setLocale(getSharedPreferences("Ajustes", MODE_PRIVATE).
+                getString("Idioma", "es"), false);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -33,6 +44,49 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.idioma_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.idioma_es) {
+            setLocale("es", true);
+            return true;
+        } else if (id == R.id.idioma_eu) {
+            setLocale("eu", true);
+            return true;
+        } else if (id == R.id.idioma_en) {
+            setLocale("en", true);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void setLocale(String langCode, boolean recrearActividad) {
+        getSharedPreferences("Ajustes", MODE_PRIVATE)
+                .edit()
+                .putString("Idioma", langCode)
+                .apply();
+
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        // eliminar el recreate y hacer que se recarguen solo los textos.
+        if (recrearActividad){
+            recreate();
+        }
     }
 
 }
